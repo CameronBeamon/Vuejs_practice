673d9c2e594fbcf9001ce4f6c4d3beb8d8ec9361
Cameron Beamon <BeamonCameron@gmail.com>
2022-04-07 19:27:27 -0500

    started making vuejs calculator

---
+++ calculator_app/src/views/Home.vue
@@ -0,0 +1,54 @@
+<template>
+  <input type="number" v-model="answer" />
+  <div>
+    <button v-on:click="answer = 1">1</button>
+    <button v-on:click="answer = 2">2</button>
+    <button v-on:click="answer = 3">3</button>
+    <button>+</button>
+  </div>
+  <div>
+    <button v-on:click="answer = 4">4</button>
+    <button v-on:click="answer = 5">5</button>
+    <button v-on:click="answer = 6">6</button>
+    <button>-</button>
+  </div>
+  <div>
+    <button v-on:click="answer = 7">7</button>
+    <button v-on:click="answer = 8">8</button>
+    <button v-on:click="answer = 9">9</button>
+    <button>/</button>
+  </div>
+  <div>
+    <button v-on:click="answer = 0">0</button>
+    <button>Clear</button>
+    <button>*</button>
+  </div>
+  <div><button>Enter</button></div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      enter: false,
+      answer: 0,
+      numbers: [],
+      method: "",
+    };
+  },
+  methods: {
+    clear_input: function () {
+      this.numbers = [];
+      this.answer = 0;
+    },
+    save_input: function (number) {
+      this.numbers.push(number);
+      if (this.enter == true) {
+        this.calculation();
+        this.enter == false;
+      }
+    },
+    calculation: function () {},
+  },
+};
+</script>
